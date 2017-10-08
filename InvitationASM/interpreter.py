9a3db9ba7ce9d345803649067f3e5a6d2bfb5b21
Riley Flynn <riley@rileyflynn.me>
2017-10-08 19:31:55 -0230

    Added method documentation for interpreter

--- InvitationASM/interpreter.py
+++ InvitationASM/interpreter.py
@@ -5,6 +5,11 @@ parser = Parser()
 
 
 def load_file(filename: str):
+    """
+    Parses and loads a file into memory
+    
+    :param filename: The name of the file to load
+    """
     statements = parser.parse_file(filename)
 
     for statement in statements:
@@ -15,6 +20,9 @@ def load_file(filename: str):
 
 
 def run():
+    """
+    Runs the program currently loaded into memory
+    """
     while MEMORY.pc.value <= MEMORY.get_max_address():
         current_pc = MEMORY.pc.value
         try:
