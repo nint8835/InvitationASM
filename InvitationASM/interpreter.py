ba585d0be3d93070b544f9cff651f48d774bd78d
Riley Flynn <riley@rileyflynn.me>
2017-12-19 15:40:24 -0330

    Added PROPER label support

--- InvitationASM/interpreter.py
+++ InvitationASM/interpreter.py
@@ -1,18 +1,76 @@
 import logging
+from typing import List
+import re
 
 from .parser import Parser
 from .operations import TOKENS
 from .memory import MemoryValue, MEMORY
 
 
+CODE_LABEL = re.compile(r"^(\w+):$")
+DATA_LABEL = re.compile(r"^(\w+): \.data (\d+)$")
+ARGUMENTS = re.compile(r"^\w+ (.+)$")
+
+
+def preprocess_code(data: List[str]) -> List[str]:
+    labels = {}
+    data_offset = 1000
+
+    new_code = []
+
+    # Generate labels for data sections
+    for line in data:
+        if DATA_LABEL.match(line):
+            results = DATA_LABEL.findall(line)[0]
+            labels[results[0]] = data_offset
+            new_code.insert(0, f"INIT {data_offset}, {results[1]}")
+            data_offset += 1
+        else:
+            new_code.append(line)
+
+    data = new_code
+    new_code = []
+
+    data_length = len(labels)
+
+    # Generate labels for code sections
+    for i, line in enumerate(data):
+        if CODE_LABEL.match(line):
+            labels[CODE_LABEL.findall(line)[0]] = i + 1 - len(labels) + data_length
+        else:
+            new_code.append(line)
+
+    data = new_code
+    new_code = []
+
+    # Replace all label references with memory addresses
+    for line in data:
+        operation = line.split(" ")[0]
+        args = ARGUMENTS.findall(line)
+        new_args = []
+        if args:
+            args = args[0].split(", ")
+            for arg in args:
+                if arg in labels:
+                    new_args.append(str(labels[arg]))
+                else:
+                    new_args.append(arg)
+        new_code.append(operation + " " + ", ".join(new_args))
+
+    return new_code
+
+
 def load_file(filename: str, log_level: int = logging.WARNING):
     """
     Parses and loads a file into memory
 
+    :param log_level: The log level for the parser
     :param filename: The name of the file to load
     """
     parser = Parser(log_level)
-    statements = parser.parse_file(filename)
+    with open(filename) as f:
+        processed_file = preprocess_code(list(map(lambda x: x.strip(), f.readlines())))
+    statements = parser.parse_list(processed_file)
 
     for statement in statements:
         if statement.operation == TOKENS["ANCHOR"]:
